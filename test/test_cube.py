e234d1082fc60f7c7e16a44e1e6e7b1cc0f6dd6b
Norman Fomferra <norman.fomferra@gmail.com>
2021-04-21 14:30:18 +0200

    Extended test

--- test/test_cube.py
+++ test/test_cube.py
@@ -26,9 +26,9 @@ import xarray as xr
 
 from test.test_sentinelhub import HAS_SH_CREDENTIALS
 from test.test_sentinelhub import REQUIRE_SH_CREDENTIALS
-from xcube_sh.sentinelhub import SentinelHub
 from xcube_sh.config import CubeConfig
 from xcube_sh.cube import open_cube
+from xcube_sh.sentinelhub import SentinelHub
 
 cube_config = CubeConfig(dataset_name='S2L1C',
                          band_names=['B04'],
@@ -38,18 +38,27 @@ cube_config = CubeConfig(dataset_name='S2L1C',
                          time_tolerance='30M')
 
 cube_config_t1_none = CubeConfig(dataset_name='S2L1C',
-                         band_names=['B04'],
-                         bbox=(10.00, 54.27, 11.00, 54.60),
-                         spatial_res=0.00018,
-                         time_range=('2021-01-01', None),
-                         time_period='1D')
+                                 band_names=['B04'],
+                                 bbox=(10.00, 54.27, 11.00, 54.60),
+                                 spatial_res=0.00018,
+                                 time_range=('2021-01-01', None),
+                                 time_period='1D')
 
 cube_config_t_none = CubeConfig(dataset_name='S2L1C',
-                         band_names=['B04'],
-                         bbox=(10.00, 54.27, 11.00, 54.60),
-                         spatial_res=0.00018,
-                         time_range=(None,'2021-02-18'),
-                         time_tolerance='30M')
+                                band_names=['B04'],
+                                bbox=(10.00, 54.27, 11.00, 54.60),
+                                spatial_res=0.00018,
+                                time_range=(None, '2021-02-18'),
+                                time_tolerance='30M')
+
+cube_config_with_crs = CubeConfig(dataset_name='S2L1C',
+                                  band_names=['B01'],
+                                  tile_size=(512, 512),
+                                  bbox=(-1953275.571528, 1648364.470634, -1936149.179188, 1664301.688856),
+                                  crs="http://www.opengis.net/def/crs/EPSG/0/3857",
+                                  spatial_res=10,  # in meters
+                                  time_range=('2018-05-14', '2020-07-31'),
+                                  time_tolerance='30M')
 
 
 @unittest.skipUnless(HAS_SH_CREDENTIALS, REQUIRE_SH_CREDENTIALS)
@@ -61,13 +70,13 @@ class CubeTest(unittest.TestCase):
     def test_time_max_none(self):
         cube = open_cube(cube_config=cube_config_t1_none)
         self.assertIsInstance(cube, xr.Dataset)
-        self.assertEqual(np.datetime64('today', 'D'), np.datetime64(cube.time.values[-1], 'D'))        # self.assertEqual()
+        self.assertEqual(np.datetime64('today', 'D'), np.datetime64(cube.time.values[-1], 'D'))  # self.assertEqual()
 
     def test_time_none(self):
         cube = open_cube(cube_config=cube_config_t_none)
         self.assertIsInstance(cube, xr.Dataset)
-        self.assertEqual(np.datetime64('2021-02-16'), np.datetime64(cube.time.values[-1], 'D'))        # self.assertEqual()
-        self.assertEqual(np.datetime64('2015-06-27'), np.datetime64(cube.time.values[0], 'D'))        # self.assertEqual()
+        self.assertEqual(np.datetime64('2021-02-16'), np.datetime64(cube.time.values[-1], 'D'))  # self.assertEqual()
+        self.assertEqual(np.datetime64('2015-06-27'), np.datetime64(cube.time.values[0], 'D'))  # self.assertEqual()
 
 
 @unittest.skipUnless(HAS_SH_CREDENTIALS, REQUIRE_SH_CREDENTIALS)
@@ -82,18 +91,8 @@ class CubeWithCredentialsTest(unittest.TestCase):
 
     @unittest.skipUnless(HAS_SH_CREDENTIALS, REQUIRE_SH_CREDENTIALS)
     def test_open_cube_with_other_crs(self):
-        bbox_other = [-1953275.571528, 1648364.470634, -1936149.179188, 1664301.688856]
-
-        projection_code = "http://www.opengis.net/def/crs/EPSG/0/3857"
-
-        cube_config = CubeConfig(dataset_name='S2L1C',
-                                 band_names=['B01'],
-                                 tile_size=[512, 512],
-                                 bbox=bbox_other,
-                                 crs=projection_code,
-                                 spatial_res=10,  # in meters
-                                 time_range=['2018-05-14', '2020-07-31'],
-                                 time_tolerance='30M')
-
-        cube = open_cube(cube_config)
-
+        cube = open_cube(cube_config_with_crs)
+        self.assertIsInstance(cube, xr.Dataset)
+        self.assertEqual({'time': 320, 'y': 2048, 'x': 2048, 'bnds': 2}, cube.dims)
+        self.assertEqual({'x', 'y', 'time', 'time_bnds'}, set(cube.coords))
+        self.assertEqual({'B01'}, set(cube.data_vars))
